829bed780ea467e79e16e79d1d06cd69bd8ff109
Jakob Metzger <jj.metzger@gmail.com>
2017-07-02 16:54:32 -0400

    fixed more numbering issues

--- watershed3d.py
+++ watershed3d.py
@@ -232,13 +232,22 @@ class Ws3d(object):
                         ax.set_clim(0, image_to_plot.max())
 
                     # show seeds of watershed. TODO: Do it on the dataframe since may have deleted cells
-                    seeds_in_current_z = self.peaks[self.peaks[:, 0] == n][:,1:] # find seeds that are in the current z
-                    axes[i, j].plot(seeds_in_current_z[:, 1], seeds_in_current_z[:, 0], 'xr')
+                    # seeds_in_current_z = self.peaks[self.peaks[:, 0] == n][:,1:] # find seeds that are in the current z
+                    # axes[i, j].plot(seeds_in_current_z[:, 1], seeds_in_current_z[:, 0], 'xr')
 
-                    # label seeds
-                    for ipeaks in range(seeds_in_current_z.shape[0]):
-                        axes[i, j].text(seeds_in_current_z[ipeaks, 1], seeds_in_current_z[ipeaks, 0], str(peak_counter), color='r', fontsize=22)
-                        peak_counter += 1
+                    # # label seeds
+                    # for ipeaks in range(seeds_in_current_z.shape[0]):
+                    #     axes[i, j].text(seeds_in_current_z[ipeaks, 1], seeds_in_current_z[ipeaks, 0], str(peak_counter), color='r', fontsize=22)
+                    #     peak_counter += 1
+
+                    # # if show_labels:
+                    assert len(self.df) == self.peaks.shape[0]
+                    for ipeaks in range(len(self.df)):
+                        if int(np.round(self.df.iloc[ipeaks].centroid[0])) == n:
+                            axes[i,j].text(self.df.iloc[ipeaks].centroid[2], self.df.iloc[ipeaks].centroid[1],
+                                       str(self.df.iloc[ipeaks].label),
+                                       color='r', fontsize=22)
+                            axes[i,j].plot(self.df.iloc[ipeaks].centroid[2], self.df.iloc[ipeaks].centroid[1], 'xr')
 
                     axes[i, j].set_xlim(self.peaks[:, 2].min() - 20, self.peaks[:, 2].max() + 20)
                     axes[i, j].set_ylim(self.peaks[:, 1].min() - 20, self.peaks[:, 1].max() + 20)
@@ -2019,11 +2028,25 @@ def browse_stack(w):
         peak_counter = 1
         seeds_in_current_z = w.peaks[w.peaks[:, 0] == i][:, 1:]  # find seeds that are in the current z
 
-        for ipeaks in range(seeds_in_current_z.shape[0]):
-            plt.text(seeds_in_current_z[ipeaks, 1], seeds_in_current_z[ipeaks, 0], str(peak_counter), color='r', fontsize=22)
-            peak_counter += 1
+        # if show_labels:
+        #     assert len(self.df) == self.peaks.shape[0]
+        #     for ipeaks in range(len(self.df)):
+        #         ax[1].text(self.df.iloc[ipeaks].centroid[2], self.df.iloc[ipeaks].centroid[1],
+        #                    str(self.df.iloc[ipeaks].label),
+        #                    color='r', fontsize=22)
+        for ipeaks in range(len(w.df)):
+            if int(np.round(w.df.iloc[ipeaks].centroid[0])) == i:
+                plt.text(w.df.iloc[ipeaks].centroid[2], w.df.iloc[ipeaks].centroid[1],
+                                str(w.df.iloc[ipeaks].label),
+                                color='r', fontsize=22)
+                plt.plot(w.df.iloc[ipeaks].centroid[2], w.df.iloc[ipeaks].centroid[1], 'xr')
 
-        plt.plot(seeds_in_current_z[:, 1], seeds_in_current_z[:, 0], 'xr')
+
+        # for ipeaks in range(seeds_in_current_z.shape[0]):
+        #     plt.text(seeds_in_current_z[ipeaks, 1], seeds_in_current_z[ipeaks, 0], str(peak_counter), color='r', fontsize=22)
+        #     peak_counter += 1
+
+        # plt.plot(seeds_in_current_z[:, 1], seeds_in_current_z[:, 0], 'xr')
         plt.xlim(w.peaks[:, 2].min() - 20, w.peaks[:, 2].max() + 20)
         plt.ylim(w.peaks[:, 1].min() - 20, w.peaks[:, 1].max() + 20)
         plt.axis('off')
